0c8f539af9aecc80b9c166a8e25abd51ae3fe59c
bayram <bayramk@gmail.com>
2025-02-07 22:45:32 +0300

    test

--- 10_debs.sh
+++ 10_debs.sh
@@ -21,7 +21,10 @@ wget -O chroot/tmp/firefox.tar.bz2 "https://download.mozilla.org/?product=firefo
 tar -xvf chroot/tmp/firefox.tar.bz2 -C chroot/etc/skel/
 mv chroot/etc/skel/firefox chroot/etc/skel/.firefox
 chroot chroot apt install libdbus-glib-1-2 -y
-
+mkdir -p chroot/usr/lib/firefox
+cd chroot/usr/lib/firefox
+ln -s ../../../etc/skel/.firefox/firefox firefox
+cd -
 #tar xvf chroot/tmp/firefox-$VERSION.tar.bz2
 #rm chroot/tmp/firefox-$VERSION.tar.bz2
 #mv ./firefox chroot/opt/
